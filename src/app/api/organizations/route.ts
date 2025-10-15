5b729ceedb8281566769762f464ff62a4e64d652
erik-mauricio <em882@cornell.edu>
2025-10-15 14:04:28 -0400

    fully implemented and tested POST and DELETE Request

--- src/app/api/organizations/route.ts
+++ src/app/api/organizations/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createServerSupabaseClient } from "../../../../lib/supabase";
-import { validate as uuidValidate } from 'uuid';
+import { validate as uuidValidate } from "uuid";
 
 export async function GET(request: NextRequest) {
   const supabase = createServerSupabaseClient();
@@ -12,15 +12,14 @@ export async function DELETE(request: NextRequest) {
     const supabase = createServerSupabaseClient();
     const id = request.nextUrl.searchParams.get("id");
 
-
-    const convertToInt = Number(id)
-    if(!Number.isInteger(convertToInt) && !uuidValidate(id)){
+    const convertToInt = Number(id);
+    if (!id || (!Number.isInteger(convertToInt) && !uuidValidate(id))) {
       console.error("ID is not an Integer or a UUID");
       return NextResponse.json(
-        { error: "ID is not an Integer or a UUID"},
+        { error: "ID is not an Integer or a UUID or ID was Null" },
         { status: 400 }
       );
-    } 
+    }
 
     const { data: organization, error } = await supabase
       .from("organizations")
@@ -53,7 +52,7 @@ export async function DELETE(request: NextRequest) {
       { status: 200 }
     );
   } catch (e) {
-    console.error("Unexpected error in GET handler:");
+    console.error("Unexpected error in Delete handler");
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
@@ -66,23 +65,176 @@ export async function POST(request: NextRequest) {
     const supabase = createServerSupabaseClient();
     const requestData = await request.json();
 
+    const orgKinds = ["restaurant", "cbo"];
+    const nycBoroughs = [
+      "Bronx",
+      "Brooklyn",
+      "Manhattan",
+      "Queens",
+      "Staten Island",
+    ];
+    const stateBPChar = new Set(["NY"]);
+
+    const name = requestData.name;
+    if (!name || typeof requestData.name != "string") {
+      console.error(
+        "Organization name was not provided or name is not a string"
+      );
+      return NextResponse.json(
+        {
+          success: false,
+          data: null,
+          error: "Bad Request: name was not provided or it is not a string",
+        },
+        { status: 400 }
+      );
+    }
+
+    if (!requestData.org_type || !orgKinds.includes(requestData.org_type)) {
+      console.error("Invalid org type or it was not provided");
+      return NextResponse.json(
+        {
+          success: false,
+          data: null,
+          error: "Bad Request: Invalid org type or it was not provided",
+        },
+        { status: 400 }
+      );
+    }
+
+    if (
+      !requestData.number_of_meals || ( typeof requestData.number_of_meals == "string" ||
+      !Number.isInteger(Number(requestData.number_of_meals)))
+    ) {
+      console.error(
+        "Invalid type for number of meals or was it was not provided"
+      );
+      return NextResponse.json(
+        {
+          success: false,
+          data: null,
+          error: "Invalid type for number of meals or was it was not provided",
+        },
+        { status: 400 }
+      );
+    }
+
+    const street_address = requestData.street_address;
+    if (!street_address || typeof requestData.street_address != "string") {
+      console.error(
+        "Organization street address was not provided or street address is not a string"
+      );
+      return NextResponse.json(
+        {
+          success: false,
+          data: null,
+          error: "Bad Request: street address was not provided or it is not a string",
+        },
+        { status: 400 }
+      );
+    }
+
+    if (!requestData.borough || !nycBoroughs.includes(requestData.borough)) {
+      console.error(
+        "Organization borough was not provided or it is invalid or borough is not a string"
+      );
+      return NextResponse.json(
+        {
+          success: false,
+          data: null,
+          error:
+            "Bad Request: Organization borough was not provided or it is invalid or borough is not a string",
+        },
+        { status: 400 }
+      );
+    }
+
+    if (!requestData.state || !stateBPChar.has(requestData.state)) {
+      console.error("Invalid State abbreviation or it was not provided");
+      return NextResponse.json(
+        {
+          success: false,
+          data: null,
+          error:
+            "Bad Request: Invalid State abbreviation or it was not provided",
+        },
+        { status: 400 }
+      );
+    }
+
+    const zip = requestData.zip;
+    if (!zip || typeof requestData.zip != "string") {
+      console.error(
+        "Organization zip address was not provided or zip address is not a string"
+      );
+      return NextResponse.json(
+        {
+          success: false,
+          data: null,
+          error:
+            "Organization zip address was not provided or zip address is not a string",
+        },
+        { status: 400 }
+      );
+    }
+
+    const created_at = requestData.created_at;
+    const created_atTime = new Date(created_at);
+    if (
+      !created_at ||
+      (typeof created_at != "string" || isNaN(created_atTime.getTime()))
+    ) {
+      console.error("Created_at time was not provided or it is invalid");
+      return NextResponse.json(
+        {
+          success: false,
+          data: null,
+          error: "Created_at time was not provided or it is invalid",
+        },
+        { status: 400 }
+      );
+    }
+
+    const updated_at = requestData.updated_at;
+    const updatedTime = new Date(updated_at);
+    if (
+      !updated_at ||
+      (typeof created_at != "string" || isNaN(updatedTime.getTime()))
+    ) {
+      console.error("Updated_at time was not provided or it is invalid");
+      return NextResponse.json(
+        {
+          success: false,
+          data: null,
+          error: "Updated_at time was not provided or it is invalid",
+        },
+        { status: 400 }
+      );
+    }
+
     const { data: addedOrganization, error } = await supabase
       .from("organizations")
       .insert(requestData)
       .select();
 
     if (error) {
-      console.error("Error fetching items:", error.message);
+      console.error("Internal Server Error: ", error.message);
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
 
     return NextResponse.json(
       {
         success: true,
-        data: addedOrganization,
+        data: { organization: addedOrganization[0] },
         error: null,
       },
-      { status: 200 }
+      { status: 201 }
     );
-  } catch (e) {}
+  } catch (e) {
+    console.error("Unexpected error in Post handler");
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
